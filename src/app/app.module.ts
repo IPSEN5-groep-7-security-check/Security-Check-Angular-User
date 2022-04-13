e2b42d0a2fbb16820ede88ef29a254d3e791e052
Hayyan55 <hayan_mez@hotmail.co.uk>
2022-04-13 14:02:43 +0200

    De eerste commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MainScanComponent } from './main-scan/main-scan.component';
+import { AppRoutingModule } from './app-routing.module';
+import { LoadScanComponent } from './load-scan/load-scan.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainScanComponent,
+    LoadScanComponent,
+    LoadScanComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
