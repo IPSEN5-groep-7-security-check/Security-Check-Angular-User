839462b9f4e4cb9804349af489a5cfaeb7a8f096
Hayyan55 <hayan_mez@hotmail.co.uk>
2022-04-15 16:15:17 +0200

    Results view added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {MainScanComponent} from "./main-scan/main-scan.component";
 import {LoadScanComponent} from "./load-scan/load-scan.component";
+import {ScanResultComponent} from "./scan-result/scan-result.component";
 
 const routes: Routes = [
   {path:'loadScan', component: LoadScanComponent},
   {path: '', redirectTo: '/mainScan', pathMatch: 'full'},
-  { path: 'mainScan', component: MainScanComponent}
+  { path: 'mainScan', component: MainScanComponent},
+  {path: 'scanResult', component: ScanResultComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { MainScanComponent } from './main-scan/main-scan.component';
 import { AppRoutingModule } from './app-routing.module';
 import { LoadScanComponent } from './load-scan/load-scan.component';
+import { ScanResultComponent } from './scan-result/scan-result.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainScanComponent,
     LoadScanComponent,
-    LoadScanComponent
+    LoadScanComponent,
+    ScanResultComponent
   ],
   imports: [
     BrowserModule,
