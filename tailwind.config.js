8eb112fd5b08ee4bd11ec270333eafbc13feb529
Hayyan55 <62708524+Hayyan55@users.noreply.github.com>
2022-05-12 10:02:24 +0200

    Landing page (#4)
    Co-authored-by: Frank van den Broek <s1130543@student.hsleiden.nl>
    Co-authored-by: marvinalnashiedu <marvinalnashiedu@gmail.com>
    Co-authored-by: loek <loek@appel-snijder.nl>
    Co-authored-by: Marvin Al Nashi <70692080+marvinalnashiedu@users.noreply.github.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,32 +17,19 @@ module.exports = {
   daisyui: {
     themes: [
       {
-        mytheme: {
-          /* your theme name */ primary: '#f5a400' /* Primary color */,
-          'primary-focus': '#dc9300' /* Primary color - focused */,
-          'primary-content': '#ffffff' /* Foreground content color to use on primary color */,
-
-          secondary: '#1a1a1a' /* Secondary color */,
-          'secondary-focus': '#000' /* Secondary color - focused */,
-          'secondary-content': '#ffffff' /* Foreground content color to use on secondary color */,
-
-          accent: '#f5a400' /* Accent color */,
-          'accent-focus': '#2aa79b' /* Accent color - focused */,
-          'accent-content': '#ffffff' /* Foreground content color to use on accent color */,
-
-          neutral: '#3d4451' /* Neutral color */,
-          'neutral-focus': '#2a2e37' /* Neutral color - focused */,
-          'neutral-content': '#ffffff' /* Foreground content color to use on neutral color */,
-
-          'base-100': '#ffffff' /* Base color of page, used for blank backgrounds */,
-          'base-200': '#f9fafb' /* Base color, a little darker */,
-          'base-300': '#d1d5db' /* Base color, even darker */,
-          'base-content': '#1f2937' /* Foreground content color to use on base color */,
-
-          info: '#2094f3' /* Info */,
-          success: '#009485' /* Success */,
-          warning: '#ff9900' /* Warning */,
-          error: '#ff5724' /* Error */,
+        getBigMarketingTheme: {
+          primary: "#f597a9",
+          secondary: "#ffce00",
+          accent: "#30cfdb",
+          neutral: "#3a3a3c",
+          "neutral-content": "#cccccc",
+          "base-100": "#ffffff",
+          "base-200": "#f3f6fd",
+          "base-content": "#6e6e6e", // #252525 for headings
+          info: "#20aaf9",
+          success: "#00d084",
+          warning: "#F5AB32",
+          error: "#cf2e2e",
         },
       },
     ],
