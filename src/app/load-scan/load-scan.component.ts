af019588542dcca5f8a96bb4ec45f9b249e3bb66
marvinalnashiedu <marvinalnashiedu@gmail.com>
2022-05-24 11:54:55 +0200

    Update load scan component

--- src/app/load-scan/load-scan.component.ts
+++ src/app/load-scan/load-scan.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from "@angular/router";
 import {timeInterval} from "rxjs";
+import {HTTPService} from "../services/h-t-t-p.service";
 
 @Component({
   selector: 'app-load-scan',
@@ -9,16 +10,23 @@ import {timeInterval} from "rxjs";
 })
 export class LoadScanComponent implements OnInit {
 
-  constructor(private router: Router,
-              ) { }
-  //private http: BESTAATNOGNIETINDEZEBRANCH
+  constructor(public router: Router, private httpService: HTTPService) { }
 
   ngOnInit(): void {
     setTimeout(() => {
       this.router.navigate(['/scan-results']);
     }, 15000);
-    console.log(timeInterval())
+    this.checkScanStatus();
   }
+  
+  checkScanStatus() {
+    this.httpService.getScanStatus().subscribe(data => {
+      console.log(data);
+      return data;
+    });
+  }
+
+
 
   // checkScan(): void {
   //   while(this.http.getScanState() != "FINISHED") { //pluck("state")?
