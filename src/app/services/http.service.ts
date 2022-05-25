aaa327d5e7367e0e1d56833341436cd9d859540c
Frank van den Broek <s1130543@student.hsleiden.nl>
2022-05-25 14:51:23 +0200

    post /analyze

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -10,11 +10,19 @@ import { Report } from '@prisma/client';
 export class HTTPService {
   constructor(private http: HttpClient) {}
 
-  postScanUrl(url: AbstractControl) {
-    return this.http.post('localhost:8080' + '/api/v1/analyze', url);
-    console.log('yay');
-    console.log(url);
+  startScan(host: string, rescan: boolean | null) {
+    return this.http.post(environment.apiUrl + '/api/v1/analyze', null, {
+      params: {
+        host: host,
+        rescan: rescan ? 'true' : 'false',
+      },
+    });
   }
+
+  postScanUrl(url: String) {
+    return this.http.post('localhost:8080' + '/api/v1/analyze', url);
+  }
+
   getScanStatus() {
     return this.http.get(environment.apiUrl + '/api/v1/analyze');
   }
