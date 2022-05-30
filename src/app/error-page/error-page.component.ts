760d6ec1706abe71e388828f3dabbd803b0379b7
Loek <loek@appel-snijder.nl>
2022-05-30 10:42:34 +0200

    opschoning

--- src/app/error-page/error-page.component.ts
+++ src/app/error-page/error-page.component.ts
@@ -7,30 +7,20 @@ import {ActivatedRoute, Data} from "@angular/router";
   styleUrls: ['./error-page.component.css']
 })
 export class ErrorPageComponent implements OnInit {
-  errorMessage!: string;
-
-  delay = (ms: number) => new Promise(res => setTimeout(res, ms));
-
-  pickedPhoto: any;
+  pickedPhoto?: string;
 
   errorPhotos: string[] = [
     "assets/404Photos/error_01.webp",
     "assets/404Photos/error_02.jpg"
   ];
-
-  constructor(private route: ActivatedRoute) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.route.data.subscribe(
-      (data: Data) => {
-        this.errorMessage = data['message'];
-        this.pickedPhoto = this.randomErrorPhoto();
-      }
-    );
+    this.pickedPhoto = this.randomErrorPhoto();
   }
 
-  public randomErrorPhoto() {
-    let randomIndex = Math.floor(Math.random() * (this.errorPhotos.length-1));
+  randomErrorPhoto() {
+    let randomIndex = Math.floor(Math.random() * this.errorPhotos.length);
     return this.errorPhotos[randomIndex];
   }
 }
