0eed9d571ba74f4a4fb20c45baf4e374d8daa4ec
Loek <loek@appel-snijder.nl>
2022-06-15 10:54:38 +0200

    einde error page testing

--- src/app/error-page/error-page.component.spec.ts
+++ src/app/error-page/error-page.component.spec.ts
@@ -1,29 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ErrorPageComponent } from './error-page.component';
-import assert from "assert";
-
 describe('ErrorPageComponent', () => {
   let component: ErrorPageComponent;
   let fixture: ComponentFixture<ErrorPageComponent>;
-
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ErrorPageComponent],
     }).compileComponents();
   });
-
   beforeEach(() => {
     fixture = TestBed.createComponent(ErrorPageComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-  it('pickedPhoto number should be random and betweeen 0 and 1',() => {
-    let randomNumber = component.randomErrorPhoto();
-    assert(randomNumber != 0 && randomNumber >=0 && randomNumber <= 1);
+  it('picked random photoNumber greater than or equal to 0',() => {
+    let photoNumber = component.randomErrorPhoto();
+    expect(photoNumber).toBeGreaterThanOrEqual(0);
+  })
+  it('picked random photoNumber smaller than 1',() => {
+    let photoNumber = component.randomErrorPhoto();
+    expect(photoNumber).toBeLessThan(1)
   })
 });
