96c2c5fff698981af4e692604de0899e3b1dcc69
marvinalnashiedu <marvinalnashiedu@gmail.com>
2022-06-21 22:20:08 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,9 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import { environmentprod } from './environments/environment.prod';
 
-if (environment.production) {
+if (environmentprod.production) {
   enableProdMode();
 }
 
