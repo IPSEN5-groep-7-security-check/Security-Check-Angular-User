ac8671b56866659659cd383ad9437cd1d8b550fc
marvinalnashiedu <marvinalnashiedu@gmail.com>
2022-06-22 00:05:20 +0200

    Revert "Update main.ts"
    This reverts commit 96c2c5fff698981af4e692604de0899e3b1dcc69.

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,9 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-import { environmentprod } from './environments/environment.prod';
+import { environment } from './environments/environment';
 
-if (environmentprod.production) {
+if (environment.production) {
   enableProdMode();
 }
 
