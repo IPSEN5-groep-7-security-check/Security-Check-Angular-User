796ef242bff1c73d351451ed81f58d837b6734a0
Loek <loek@appel-snijder.nl>
2022-06-27 10:24:48 +0200

    titel naar IPSEN5-groep07-GBM

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true, // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require("path").join(__dirname, "./coverage/ipsen4-demo"),
+      dir: require("path").join(__dirname, "./coverage/IPSEN5-groep07-GBM"),
       subdir: ".",
       reporters: [{ type: "html" }, { type: "text-summary" }],
     },
